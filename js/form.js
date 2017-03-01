8e750e082b22dd6f7050a4235f45766f077fa291
Alex Bush <alexandrabush1@gmail.com>
2017-03-01 14:29:36 -0500

    Style guide fixes

--- js/form.js
+++ js/form.js
@@ -1,6 +1,5 @@
 $(function(){
-    $('#send-message').on('click', function (e) {
-
+    $('#sendMessage').on('click', function (e) {
         alert('Sorry, we cannot send your message right now. This feature is coming soon!');
     })
 });
